c6d45205aa39ba122575b37e33d9b01650e38e53
Daniel Codrea <daniel.codrea@gmail.com>
2015-03-08 00:37:15 -0800

    First layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -30,3 +30,50 @@ $('.navbar-brand').click(function(){
 $('.navbar-collapse li a').click(function(){
 	$('.navbar-collapse').removeClass('in');
 });
+
+$('.flexslider').flexslider({
+    animation: 'fade',
+    easing: 'jswing',
+    controlNav: false,
+    pauseOnHover: false,
+    directionNav: false,
+    slideshowSpeed: 8000,
+    animationSpeed: 1800,
+    start: function ( slider ) {
+        $('li .caption_1').addClass('animated bounceInLeft');
+    },
+    before: function ( slider ) {
+        if(slider.currentSlide === 0) {
+            $('.caption_1').removeClass('animated bounceInLeft');
+            $('.caption_2').addClass('animated bounceInRight');
+            $('.caption_3').removeClass('animated rotateInDownLeft');
+            $('.caption_4').removeClass('animated rollIn');
+        } else if(slider.currentSlide === 1) {
+            $('.caption_1').removeClass('animated bounceInLeft');
+            $('.caption_2').removeClass('animated bounceInRight');
+            $('.caption_3').addClass('animated rotateInDownLeft');
+            $('.caption_4').removeClass('animated rollIn');
+        } else if(slider.currentSlide === 2) {
+            $('.caption_1').removeClass('animated bounceInLeft');
+            $('.caption_2').removeClass('animated bounceInRight');
+            $('.caption_3').removeClass('animated rotateInDownLeft');
+            $('.caption_4').addClass('animated rollIn');
+        } else if(slider.currentSlide === 3) {
+            $('.caption_1').addClass('animated bounceInLeft');
+            $('.caption_2').removeClass('animated bounceInRight');
+            $('.caption_3').removeClass('animated rotateInDownLeft');
+            $('.caption_4').removeClass('animated rollIn');
+        }
+
+
+    }
+});
+
+function run() {
+    $('.progress_bar').animate({'width': '100%'}, 8000, run).width(0);
+}
+run();
+
+$('#carousel-clients').carousel({
+    interval: 1500 
+})
